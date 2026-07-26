7f95f70171645d346b10dc9b2f027b2f2e8556f4
Tanya Singh <singhtanyaofficial@gmail.com>
2026-07-27 03:10:16 +0530

    Implement secure user registration with bcrypt

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request 
+from flask_bcrypt import Bcrypt
 from config import Config
-from models.user import db
+from models.user import db, User
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bcrypt = Bcrypt(app)
 
 db.init_app(app)
 
@@ -17,8 +19,25 @@ def home():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        return "submitted!"
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
+        if existing_user:
+            return "Username or email already exists. Please choose a different one."
+        
+        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+        new_user = User(username=username, email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        print("=" * 20)
+        print(f"Username: {username}")
+        print(f"Email: {email}")
+        print(f"Password: {hashed_password}")
+        print("=" * 20)
+        return "User registered successfully!"
     return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
