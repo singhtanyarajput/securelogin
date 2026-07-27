bb6d64dcbfdfa1fb3fec82635d5120c927bf017d
Tanya Singh <singhtanyaofficial@gmail.com>
2026-07-27 19:03:59 +0530

    Authentication Implemented

--- app.py
+++ app.py
@@ -37,6 +37,19 @@ def register():
         print("=" * 20)
         return "User registered successfully!"
     return render_template("register.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            return "Invalid Username"
+        if user and bcrypt.check_password_hash(user.password, password):
+            return "Login successful!"
+        else:
+            return "Invalid username or password."
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
